6c9e3dd07552bb3f48d3f3f8d7305b738c190eff
Qiwei <lin_qiwei@u.nus.edu>
2021-11-20 17:03:18 +0800

    Update __init__.py

--- gym_pm/__init__.py
+++ gym_pm/__init__.py
@@ -4,11 +4,16 @@ from gym.envs.registration import register
 logger = logging.getLogger(__name__)
 
 register(
-    id='Rail-v0',
+    id='Rail-v1',
     entry_point='gym_pm.envs:Rail_Env'
 )
 
 register(
-    id='Assembly-v0',
+    id='Rail-v2',
+    entry_point='gym_pm.envs:Railv2_Env'
+)
+
+register(
+    id='Assembly-v1',
     entry_point='gym_pm.envs:Assembly_Env'
 )
