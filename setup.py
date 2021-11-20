ffe07a4de04e28b56924ad93c12c74e1a6f2aa5e
Qiwei <lin_qiwei@u.nus.edu>
2021-11-20 17:02:19 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gym_pm',
-      version='0.0.1',
+      version='1.0.0',
       author="Lin Qiwei",
       install_requires=["gym", "pandas", "IPython", "reliability"]
 )
