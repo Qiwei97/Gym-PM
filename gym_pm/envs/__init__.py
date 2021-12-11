c6ac68323847c7ad62d1ec612c45a4ecfaa1e480
Qiwei <lin_qiwei@u.nus.edu>
2021-12-11 15:50:18 +0800

    Update __init__.py

--- gym_pm/envs/__init__.py
+++ gym_pm/envs/__init__.py
@@ -1,2 +1,2 @@
 from gym_pm.envs.RollingStockEnv import Rail_Env, Railv2_Env
-from gym_pm.envs.ManufacturingEnv import Assembly_Env
+from gym_pm.envs.ManufacturingEnv import Assembly_Env, Assemblyv2_Env
