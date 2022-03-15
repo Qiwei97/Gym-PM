e0c4c01f9168b053821f5be83b10afa6e0364478
Lin Qiwei <linqw97@hotmail.com>
2022-03-16 00:12:14 +0800

    Amend Data Path

--- gym_pm/utils.py
+++ gym_pm/utils.py
@@ -18,31 +18,7 @@ def create_data(Type='PdM2', split='Train', save=True):
     file_path = 'Gym-PM/gym_pm/data/' + Type + '.csv'
     df = pd.read_csv(file_path)
 
-    if Type == 'PdM1':
-
-        df = df.iloc[:, :-7]
-        df['age'] = df['Hours Since Previous Failure']
-        df.Date = pd.to_datetime(df.Date)
-        df = df.sort_values('Date')
-        df = df.drop(columns = ['ID', 'Operator', 'Hours Since Previous Failure', 'Date'])
-        df.Failure = df.Failure.apply(lambda x: 0 if x == 'No' else 1)
-        df.reset_index(drop = True, inplace = True)
-
-        # ttf
-        failure_time = np.array(sorted(df[df.Failure == 1].index.tolist()))
-        failure_list = []
-        for i in range(len(df)):
-            failure_list.append(failure_time)
-
-        df['ttf'] = failure_list
-        df.ttf = df.ttf - df.index
-        df.ttf = df.ttf.apply(lambda x: x[x >= 0]) # Drop negative values
-        df = df[df.ttf.str.len() > 0] # Drop empty lists
-        df.ttf = df.ttf.apply(lambda x: x[0])
-        df = df[~(df.age + df.ttf <= 10)]
-        df.reset_index(drop = True, inplace = True)
-
-    elif Type == 'PdM2':
+    if Type == 'PdM2':
 
         df['Failure'] = df.Failure_today.apply(lambda x: 0 if x == 'No' else 1)
         df.Date = pd.to_datetime(df.Date)
